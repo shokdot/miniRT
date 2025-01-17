6f7e533f15eba1cfa654202d27af1dfb0a02017c
Hen Aleksanian (Shok.) <henula09106@gmail.com>
2025-01-17 17:30:44 +0400

    fix(free): line_map.c for freeing matrix

--- srcs/utils/free_line_map.c
+++ srcs/utils/free_line_map.c
@@ -6,7 +6,7 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 00:13:06 by healeksa          #+#    #+#             */
-/*   Updated: 2025/01/16 00:13:14 by healeksa         ###   ########.fr       */
+/*   Updated: 2025/01/17 17:28:04 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 
 void	free_line_map(char **line, t_tracer_ptr tracer)
 {
-	ft_free((void **)line);
+	free_matrix(line);
+	line = NULL;
 	close(tracer->fd);
 	ft_free((void **)&tracer);
 }
