e807a7df979a73563e9915fcc944207fa7161b81
Hen Aleksanian (Shok.) <henula09106@gmail.com>
2025-02-12 15:57:20 +0400

    fix(parse_cords): matrix leaks

--- srcs/parsing/parse_cords.c
+++ srcs/parsing/parse_cords.c
@@ -6,7 +6,7 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/10 10:31:26 by healeksa          #+#    #+#             */
-/*   Updated: 2025/02/12 14:57:40 by healeksa         ###   ########.fr       */
+/*   Updated: 2025/02/12 15:56:24 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,9 @@ bool	parse_cords(char *line)
 	i = 0;
 	splited_line = ft_split(line, ',');
 	if (count_token(splited_line) != CORDS_TOKEN)
-		return (false);
+		return (free_matrix(splited_line), false);
 	while (i < CORDS_TOKEN)
 		if (!is_valid_cords(splited_line[i++]))
-			return (false);
-	return (true);
+			return (free_matrix(splited_line), false);
+	return (free_matrix(splited_line), true);
 }
