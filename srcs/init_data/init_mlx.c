69d62104b1198435b719e44afc7f5bbe8583baec
Hen Aleksanian (Shok.) <henula09106@gmail.com>
2025-02-25 14:42:06 +0400

    fix(init_mlx): malloc sizeof t_mlx instead of t_mlx_ptr

--- srcs/init_data/init_mlx.c
+++ srcs/init_data/init_mlx.c
@@ -6,7 +6,7 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/09 20:04:45 by healeksa          #+#    #+#             */
-/*   Updated: 2025/02/23 17:01:15 by healeksa         ###   ########.fr       */
+/*   Updated: 2025/02/25 14:41:16 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	init_mlx(t_tracer_ptr tracer)
 {
-	tracer->mlx = (t_mlx_ptr)ft_malloc(sizeof(t_mlx_ptr));
+	tracer->mlx = (t_mlx_ptr)ft_malloc(sizeof(t_mlx));
 	tracer->mlx->mlx = mlx_init();
 	tracer->mlx->mlx_win = mlx_new_window(tracer->mlx->mlx, WIDTH, HEIGHT,
 			"miniRT");
