b583b8f8be8290be37529c3d350e13e35c33e924
Hen Aleksanian (Shok.) <henula09106@gmail.com>
2025-02-27 23:28:31 +0400

    fix: compile errors

--- srcs/init_data/init_ray.c
+++ srcs/init_data/init_ray.c
@@ -6,17 +6,20 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 19:32:26 by tigran            #+#    #+#             */
-/*   Updated: 2025/02/27 23:20:43 by healeksa         ###   ########.fr       */
+/*   Updated: 2025/02/27 23:23:09 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <miniRT.h>
 
-t_ray_ptr	init_ray(void)
+t_ray_ptr	init_ray(t_vec3 origin, t_vec3 direction)
 {
 	t_ray_ptr	res;
 
 	res = (t_ray_ptr)malloc(sizeof(t_ray));
-	res = (t_ray_ptr)ft_malloc(sizeof(t_ray));
+	res->direction = (t_vec3_ptr)malloc(sizeof(t_vec3));
+	res->origin = (t_vec3_ptr)malloc(sizeof(t_vec3));
+	*(res->direction) = direction;
+	*(res->origin) = origin;
 	return (res);
 }
