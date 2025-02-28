44dfc404d5aa17f7f6568611efcd7d1df832e3bc
Hen Aleksanian (Shok.) <henula09106@gmail.com>
2025-02-28 14:31:35 +0400

    fix(init): NULL check for MLX lib, malloc -> ft_malloc

--- srcs/init_data/init_ray.c
+++ srcs/init_data/init_ray.c
@@ -6,7 +6,7 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 19:32:26 by tigran            #+#    #+#             */
-/*   Updated: 2025/02/27 23:23:09 by healeksa         ###   ########.fr       */
+/*   Updated: 2025/02/28 14:29:52 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ t_ray_ptr	init_ray(t_vec3 origin, t_vec3 direction)
 	t_ray_ptr	res;
 
 	res = (t_ray_ptr)malloc(sizeof(t_ray));
-	res->direction = (t_vec3_ptr)malloc(sizeof(t_vec3));
-	res->origin = (t_vec3_ptr)malloc(sizeof(t_vec3));
+	res->direction = (t_vec3_ptr)ft_malloc(sizeof(t_vec3));
+	res->origin = (t_vec3_ptr)ft_malloc(sizeof(t_vec3));
 	*(res->direction) = direction;
 	*(res->origin) = origin;
 	return (res);
