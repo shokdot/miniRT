0d8ffb37d9950b5e925569b657b2f9ae7f651450
Hen Aleksanian (Shok.) <henula09106@gmail.com>
2025-03-04 20:14:24 +0400

    refactor(intersect_sphere): calc cofficent for descriminant

--- srcs/intersection/intersect_sphere.c
+++ srcs/intersection/intersect_sphere.c
@@ -6,31 +6,40 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 17:08:38 by tigran            #+#    #+#             */
-/*   Updated: 2025/03/04 17:42:32 by healeksa         ###   ########.fr       */
+/*   Updated: 2025/03/04 18:44:15 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <miniRT.h>
 
-double	intersect_sphere(t_ray ray, t_sphere_ptr sphere)
+static t_vec3	calculate_cofficent(t_ray ray, t_sphere_ptr sphere)
 {
 	t_vec3	oc;
 	double	a;
 	double	b;
 	double	c;
-	double	sqrt_discriminant;
-	double	t1;
-	double	t2;
 
 	oc = vec3_sub(*(ray.origin), *(sphere->cords));
 	a = vec3_dot(*(ray.direction), *(ray.direction));
 	b = 2.0 * vec3_dot(*(ray.direction), oc);
 	c = vec3_dot(oc, oc) - sphere->diameter;
-	sqrt_discriminant = calculate_discriminant(a, b, c);
-	if (sqrt_discriminant < 0)
+	return ((t_vec3){a, b, c});
+}
+
+double	intersect_sphere(t_ray ray, t_sphere_ptr sphere)
+{
+	double	discriminant;
+	double	t1;
+	double	t2;
+	t_vec3	cofficent;
+
+	cofficent = calculate_cofficent(ray, sphere);
+	discriminant = calculate_discriminant(cofficent.x, cofficent.y,
+			cofficent.z);
+	if (discriminant < 0)
 		return (-1);
-	t1 = (-b - sqrt_discriminant) / (2 * a);
-	t2 = (-b + sqrt_discriminant) / (2 * a);
+	t1 = (-cofficent.y - discriminant) / (2 * cofficent.x);
+	t2 = (-cofficent.y + discriminant) / (2 * cofficent.x);
 	if (t1 > 0 && t2 > 0)
 		return (fmin(t1, t2));
 	else if (t1 > 0)
