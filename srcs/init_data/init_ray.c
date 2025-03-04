95d1511bf33424723194207b94469bcc61e2d807
Hen Aleksanian (Shok.) <henula09106@gmail.com>
2025-03-04 15:51:02 +0400

    fix(init_ray): malloc -> ft_malloc

--- srcs/init_data/init_ray.c
+++ srcs/init_data/init_ray.c
@@ -6,7 +6,7 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 19:32:26 by tigran            #+#    #+#             */
-/*   Updated: 2025/02/28 14:29:52 by healeksa         ###   ########.fr       */
+/*   Updated: 2025/03/04 15:48:28 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_ray_ptr	init_ray(t_vec3 origin, t_vec3 direction)
 {
 	t_ray_ptr	res;
 
-	res = (t_ray_ptr)malloc(sizeof(t_ray));
+	res = (t_ray_ptr)ft_malloc(sizeof(t_ray));
 	res->direction = (t_vec3_ptr)ft_malloc(sizeof(t_vec3));
 	res->origin = (t_vec3_ptr)ft_malloc(sizeof(t_vec3));
 	*(res->direction) = direction;
