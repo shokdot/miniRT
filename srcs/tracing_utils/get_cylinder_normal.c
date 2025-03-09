cf6e4f770f661add2d77bc1ab46cc83d79a6b8d8
Hen Aleksanian (Shok.) <henula09106@gmail.com>
2025-03-09 22:06:42 +0400

    refactor: norme

--- srcs/tracing_utils/get_cylinder_normal.c
+++ srcs/tracing_utils/get_cylinder_normal.c
@@ -6,7 +6,7 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 18:14:06 by healeksa          #+#    #+#             */
-/*   Updated: 2025/03/04 18:30:08 by healeksa         ###   ########.fr       */
+/*   Updated: 2025/03/09 22:03:31 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,22 +18,18 @@ t_vec3	get_cylinder_normal(t_cylinder_ptr obj, t_vec3 hit_point)
 	double	proj;
 	t_vec3	axis_point;
 	t_vec3	lateral_normal;
-	double	half_height;
 	t_vec3	norm_cylinder;
 
-	cp = vec3_sub(hit_point, *((t_cylinder_ptr)obj)->cords);
-	norm_cylinder = vec3_norm(*(((t_cylinder_ptr)obj)->norm));
+	cp = vec3_sub(hit_point, *(obj->cords));
+	norm_cylinder = vec3_norm(*(obj->norm));
 	proj = vec3_dot(cp, norm_cylinder);
-	axis_point = vec3_add(*(((t_cylinder_ptr)obj)->cords),
-			vec3_scale(norm_cylinder, proj));
+	axis_point = vec3_add(*(obj->cords), vec3_scale(norm_cylinder, proj));
 	lateral_normal = vec3_norm(vec3_sub(hit_point, axis_point));
-	half_height = ((t_cylinder_ptr)obj)->height / 2.0;
-	if (fabs(proj) >= half_height - EPSILION)
+	if (fabs(proj) >= (obj->height / 2.0) - EPSILION)
 	{
 		if (proj > 0)
 			return (norm_cylinder);
-		else
-			return (vec3_negate(norm_cylinder));
+		return (vec3_negate(norm_cylinder));
 	}
 	return (lateral_normal);
 }
