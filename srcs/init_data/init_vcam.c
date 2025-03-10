3bbfdf2f890251c23777f4e89a9793236caa749e
Hen Aleksanian (Shok.) <henula09106@gmail.com>
2025-03-10 17:06:38 +0400

    fix(norm): init_vcam

--- srcs/init_data/init_vcam.c
+++ srcs/init_data/init_vcam.c
@@ -6,7 +6,7 @@
 /*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 16:29:01 by healeksa          #+#    #+#             */
-/*   Updated: 2025/03/10 16:45:52 by healeksa         ###   ########.fr       */
+/*   Updated: 2025/03/10 17:05:22 by healeksa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,14 @@
 
 void	init_vcam(t_tracer_ptr tracer)
 {
+	t_vec3	cross;
+
 	tracer->scene->vcam = (t_vcam_ptr)ft_malloc(sizeof(t_vcam));
 	tracer->scene->vcam->forward = *(tracer->scene->camera->norm);
 	tracer->scene->vcam->up_ref = (t_vec3){0, 1, 0};
-	tracer->scene->vcam->right = vec3_norm(vec3_cross(tracer->scene->vcam->forward,
-				tracer->scene->vcam->up_ref));
+	cross = vec3_cross(tracer->scene->vcam->forward,
+			tracer->scene->vcam->up_ref);
+	tracer->scene->vcam->right = vec3_norm(cross);
 	tracer->scene->vcam->up = vec3_norm(vec3_cross(tracer->scene->vcam->right,
 				tracer->scene->vcam->forward));
 }
