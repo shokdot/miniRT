e98758ba3284307244f481884685f50ff5416e18
Tigran Yavroyan <tyavroya@c1r2s6.42yerevan.am>
2025-03-11 19:59:38 +0400

    chore(maps): added new maps

--- srcs/main.c
+++ srcs/main.c
@@ -3,19 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: healeksa <healeksa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tyavroya <tyavroya@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/06 15:00:43 by healeksa          #+#    #+#             */
-/*   Updated: 2025/03/10 18:15:59 by healeksa         ###   ########.fr       */
+/*   Updated: 2025/03/11 19:41:29 by tyavroya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <miniRT.h>
 
+void	gegh(void)
+{
+	system("leaks miniRT");
+}
+
 int	main(int argc, char **argv)
 {
 	t_tracer_ptr	tracer;
 
+	atexit(gegh);
 	args_check(argc, argv);
 	tracer = init_data();
 	parsing(argv[1], tracer);
